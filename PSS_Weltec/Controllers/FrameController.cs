b841aaedb3947f42c905f5d2c6985857c11d45da
FuXiong <fxcuog@gmail.com>
2015-08-05 23:31:44 +1200

    Frame

---
+++ PSS_Weltec/Controllers/FrameController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSS_Weltec.Controllers
+{
+    public class FrameController : Controller
+    {
+        //
+        // GET: /Frame/
+
+        public ActionResult FrameIndex()
+        {
+            return View();
+        }
+
+    }
+}
