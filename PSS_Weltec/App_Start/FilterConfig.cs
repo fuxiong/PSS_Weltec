41a9dc9431288d27ef9c422acdea1a2ef8de0b80
FuXiong <fxcuog@gmail.com>
2015-08-05 20:55:27 +1200

    This is empty.

---
+++ PSS_Weltec/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PSS_Weltec
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PSS_Weltec/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PSS_Weltec.MvcApplication" Language="C#" %>
